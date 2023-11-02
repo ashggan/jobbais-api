b4c4e26b35b31f4a3be92557e14f9263fee59054
Ashgan <ashganwiki@gmail.com>
2023-11-02 17:43:43 +0200

    models

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
+from happytransformer import HappyTextToText, TTSettings
 
 
 app = FastAPI()
 
-# Create a Pydantic model for the request body
-
 tokenizer = AutoTokenizer.from_pretrained("saurabhg2083/model_bert")
 model = AutoModelForSequenceClassification.from_pretrained(
     "saurabhg2083/model_bert")
+happy_tt = HappyTextToText("T5", "vennify/t5-base-grammar-correction")
+args = TTSettings(num_beams=5, min_length=1)
+
+
+# Create a Pydantic model for the request body
 
 
 class Item(BaseModel):
