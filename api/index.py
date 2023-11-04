2999ec8776382b212a8f7691d1343dd97021822f
Ashgan <ashganwiki@gmail.com>
2023-11-04 17:25:02 +0200

    post request

--- api/index.py
+++ api/index.py
@@ -14,3 +14,16 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+
+@app.route('/post', methods=['POST'])
+def handle_post():
+    data = request.get_json()  # Get the JSON data from the request
+
+    # Process the data or perform any desired operations
+    # For example, let's assume the JSON data contains a 'name' field
+    name = data.get('name')
+    if name:
+        return f"Hello, {name}! This is a POST request."
+    else:
+        return "No name provided in the request."
